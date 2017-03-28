85bbd9629daf400e96d94a829c8427de5fc3fdae
Antonio Aguilar <aaguilarbravo@gmail.com>
2017-03-27 23:25:43 -0600

    Adding load_conv.sh

---
+++ assignment_2/conv/load_conv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make -C /lib/modules/$(uname -r)/build M=$PWD modules
+sudo insmod ./conv.ko
+#cat /proc/devices | egrep conv  ; to verify if driver is loaded   
+#less /var/log/syslog    ; to see printk
+major=$( cat /proc/devices | grep char_conv  |  sed 's/[^0-9]*//g' )
+sudo mknod -m 666 /dev/conv c $major 0
+#sudo rmmod conv 
