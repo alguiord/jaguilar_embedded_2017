932ae81a324f13622e9d63c86c4e8e9b8975bd8b
Antonio Aguilar <aaguilarbravo@gmail.com>
2017-04-02 17:35:39 -0600

    Including ioctl code, still not working

--- assignment_2/test/conv/src/conv.c
+++ assignment_2/test/conv/src/conv.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/ioctl.h>  /* ioctl */
+
+#include "chardev.h"
+
 
 #define PNGSUITE_PRIMARY
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -98,6 +102,18 @@ int main(int argc, char **argv)
 	printf("3\n");
 
 
+
+  	int file_desc, ret_val;
+  	char *msg = "Message passed by ioctl\n";
+
+        device_check();
+
+  	ioctl_get_nth_byte(file_desc);
+  	ioctl_get_msg(file_desc);
+  	ioctl_set_msg(fd, msg);
+
+  	close(fd);
+
 	//run_kernel(in_image, out_image, kernel);
 
 	return 0;
@@ -153,6 +169,67 @@ void read_image_from_device(char *dstfile){
         close(fd);
 	
 }
+
+/* Functions for the ioctl calls */
+
+ioctl_set_msg(int file_desc, char *message)
+{
+  int ret_val;
+
+  ret_val = ioctl(file_desc, IOCTL_SET_MSG, message);
+
+  if (ret_val < 0) {
+    printf ("ioctl_set_msg failed:%d\n", ret_val);
+    exit(-1);
+  }
+}
+
+
+ioctl_get_msg(int file_desc)
+{
+  int ret_val;
+  char message[100]; 
+
+  /* Warning - this is dangerous because we don't tell 
+   * the kernel how far it's allowed to write, so it 
+   * might overflow the buffer. In a real production 
+   * program, we would have used two ioctls - one to tell
+   * the kernel the buffer length and another to give 
+   * it the buffer to fill
+   */
+  ret_val = ioctl(file_desc, IOCTL_GET_MSG, message);
+
+  if (ret_val < 0) {
+    printf ("ioctl_get_msg failed:%d\n", ret_val);
+    exit(-1);
+  }
+
+  printf("get_msg message:%s\n", message);
+}
+
+ioctl_get_nth_byte(int file_desc)
+{
+  int i;
+  char c;
+
+  printf("get_nth_byte message:");
+
+  i = 0;
+  while (c != 0) {
+    c = ioctl(file_desc, IOCTL_GET_NTH_BYTE, i++);
+
+    if (c < 0) {
+      printf(
+      "ioctl_get_nth_byte failed at the %d'th byte:\n", i);
+      exit(-1);
+    }
+
+    putchar(c);
+  } 
+  putchar('\n');
+}
+
+
 
 
 //This method will print the autors information
